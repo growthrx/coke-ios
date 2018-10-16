917bd651a446d2bf5180d919e03dc5dd525a03a0
Rahul Goyal1 <rahul.goyal1@timesinternet.in>
2018-10-16 15:31:16 +0530

    new files added

---
+++ Example/Pods/Target Support Files/coke-ios/coke-ios-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_coke_ios : NSObject
+@end
+@implementation PodsDummy_coke_ios
+@end
---
+++ Example/Tests/Tests.m
@@ -0,0 +1,35 @@
+//
+//  coke-iosTests.m
+//  coke-iosTests
+//
+//  Created by rahul.goyal1@timesinternet.in on 10/16/2018.
+//  Copyright (c) 2018 rahul.goyal1@timesinternet.in. All rights reserved.
+//
+
+@import XCTest;
+
+@interface Tests : XCTestCase
+
+@end
+
+@implementation Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
+
